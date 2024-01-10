f3235f8fc1939508369cca07b8e235baeb2b123f
Tomas Valli <tomas@valliconsulting.se>
2024-01-10 09:27:12 +0100

    Initial

---
+++ precache-manifest.e800ea5b9cf3c4c79f224384bf73485d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1568837b97120cd49796663f083f071f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "174c53e2658c2af22daf",
+    "url": "./static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "da43408c2f9a28febf3f",
+    "url": "./static/css/main.8653b40b.chunk.css"
+  },
+  {
+    "revision": "174c53e2658c2af22daf",
+    "url": "./static/js/2.8c41a3ed.chunk.js"
+  },
+  {
+    "revision": "ab4784922d7f45cc6cfd5a83588623bb",
+    "url": "./static/js/2.8c41a3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da43408c2f9a28febf3f",
+    "url": "./static/js/main.eacc6cf9.chunk.js"
+  },
+  {
+    "revision": "b3287891d2c1f1cb6915",
+    "url": "./static/js/runtime-main.590b0372.js"
+  }
+]);
